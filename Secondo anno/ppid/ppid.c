61789139a860c26deac4c845f90638c1b2d745f1
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2022-12-02 00:00:35 +0100

    Update ppid.c

--- Secondo anno/ppid/ppid.c
+++ Secondo anno/ppid/ppid.c
@@ -1,2 +1,29 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    int pid1;
+    int pid2;
+    int pid3;
+
+    pid1 = fork();
+    pid2 = fork();
+    pid3 = fork();
+
+    if ((pid1 == 0) || (pid2 == 0) (pid3 == 0))
+    {
+        printf("Sono il processo figlio con pid:%d. ", getpid());
+        printf("Il mio papi ha il pid: %d\n.", getppid());
+        sleep(1);
+        exit(1);
+    }
+    else
+    {
+        sleep(2);
+        printf("Sono il processo padre con ppid:%d\n.", getppid()),
+
+    }
+    return 0;
+}
