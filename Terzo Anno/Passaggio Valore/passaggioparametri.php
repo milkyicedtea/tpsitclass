6805a5cdc8e14ea551f9e550329263b4827ef277
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2023-10-20 09:56:45 +0200

    Passaggio valore da input

---
+++ Terzo Anno/Passaggio Valore/passaggioparametri.php
@@ -0,0 +1,26 @@
+<body bgcolor="#4e64fa">
+<?php
+    if (isset($_POST['valore']))
+    {
+        $valore = intval($_POST['valore']);
+
+        echo "<p> Valore passato = ".$valore."</p>";
+    }
+    else
+    {
+
+        echo '<form id = "prima" action = "passaggioparametri.php" method = "POST">';
+        echo '<h1> Inserire il valore da passare </h1>';
+        echo '<input id = "valore" name = "valore" type = "text" size = "20" maxlength = "20" autofocus required>';
+        echo '<br>';
+        echo '<br>';
+        echo '<input type = "submit" id = "submit" value = "passo parametri">';
+
+        echo '</form>';
+    }
+?>
+</body>
+
+
+
+
